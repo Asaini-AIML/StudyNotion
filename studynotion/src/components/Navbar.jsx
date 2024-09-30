5361480156bd9b58a4b0577d3e1d152fa4cd6943
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2024-09-30 18:04:17 +0530

    Initial commit

---
+++ studynotion/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
+const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <Link to="/">
+
+            </Link>
+        
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
