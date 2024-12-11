137a85e51bd5ac2c253c3f961f545090ae370a6b
Anubhav Saini <anubhav.21b1531122@abes.ac.in>
2024-12-11 16:13:53 +0530

    FolderSetup2

---
+++ studynotion/Server/routes/Profile.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const { auth, isInstructor } = require("../middlewares/auth")
+const {
+  
+  updateProfile,
+  getAllUserDetails,
+  updateDisplayPicture,
+  getEnrolledCourses,
+  instructorDashboard,
+  deleteAccount,
+} = require("../controllers/Profile")
+
+// ********************************************************************************************************
+//                                      Profile routes
+// ********************************************************************************************************
+// Delet User Account
+router.delete("/deleteAccount", auth, deleteAccount)
+router.put("/updateProfile", auth, updateProfile)
+router.get("/getUserDetails", auth, getAllUserDetails)
+// Get Enrolled Courses
+router.get("/getEnrolledCourses", auth, getEnrolledCourses)
+router.put("/updateDisplayPicture", auth, updateDisplayPicture)
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
+
+module.exports = router
